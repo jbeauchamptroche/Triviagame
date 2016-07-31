a497b39df73044c8191291a145599494d468fa63
jessicabeauchamp <jbtroche@yahoo.com>
2016-07-30 22:47:29 -0400

    final commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,14 @@
 //
 
-//click start to start game 
+//click start to start game
 
 		$(document).on('click', '#start', function(){
 		console.log("Start");
 		game.startGame();
-		
+
 });
 
-//Reset on Click (User Action) 
+//Reset on Click (User Action)
 		$(document).on('click', '#reset', function(){
 		game.reset();
 });
@@ -20,37 +20,37 @@ var randomQuestions = Math.floor(Math.random()*(questions));
 var questions = [{
 	question: "What is Marilyn Monroe's real name?",
 	answers:["Marilyn Monroe", "Jean Smith", "Laura DiMaggio", "Norma Jean Baker"],
-	correctAnswer: "Norma Jean Baker"
+	correctAnswer: "Norma Jean Baker",
 }, {
 
-	question: "For which film did Marilyn Monroe get Golden Globe Award for Best Actress in a Comedy?"
+	question: "For which film did Marilyn Monroe get Golden Globe Award for Best Actress in a Comedy?",
 	answers: ["The Prince", "Blondes", "Bus Stop", "Some like it hot"],
-	correctAnswer: "Some Like it Hot"
+	correctAnswer: "Some Like it Hot",
 }, {
 
-	question: "Where was Marilyn Monroe born?"
+	question: "Where was Marilyn Monroe born?",
 	answers: ["New York", "Los Angeles", "Philadelphia", "Austin"],
-	correctAnswer: "Los Angeles"
+	correctAnswer: "Los Angeles",
 }, {
 
-	question: "When did Marilyn Monroe die?"
+	question: "When did Marilyn Monroe die?",
 	answers: ["1968", "1964", "1962", "1970"],
-	correctAnswer: "1962"
+	correctAnswer: "1962",
 }, {
 
-	question: "Happy Birthday, Mr. President was sang to what President?"
+	question: "Happy Birthday, Mr. President was sang to what President?",
 	answers: ["JFK", "FDR", "Jimmy Carter","Ford"],
-	correctAnswer: "Norma Jean Baker"
+	correctAnswer: "Norma Jean Baker",
 }];
 
 
 //game object
 
-var game = { 
+var game = {
 	correct:0,
 	incorrect:0,
 	counter:120,
-	
+
 	countdown: function(){
 		game.counter--;
 		$('#counter-number').html(game.counter);
@@ -62,33 +62,34 @@ var game = {
 	startGame:function(){
 		timer=setInterval(game.countdown, 1000);
 		$('#start').remove();
+		for (var i = 0; i < questions.length; i++) {
+		$("#panel").append('<h2>' + questions[i].question + '</h2>');
+			for (var j = 0; j < questions[i].answers.length; j++) {
+		$("#panel").append('<input type="radio" name="question' + '-' + i + '" value="' + questions[i].answers[j] + '">' + questions[i].answers[j]);
+
+		 }
+		}
 },
 	timeUP:function() {
 		clearInterval (timer);
 		alert('Times Up!');
-		
+
 },
 	reset:function() {
 	location.reload();
 
 },
-	for (var i = 0; i < questions.length; i++) {
-    panel.append('<h2>' + questions[i].question + '</h2>');
-    for (var j = 0; j < questions[i].answers.length; j++) {
-    panel.append('<input type="radio" name="question' + '-' + i + '" value="' + questions[i].answers[j] + '">' + questions[i].answers[j]);
 
-  }
-	},
 	done: function(){
 		$.each($("imput[name='answer0']:checked"), function (){
 			console.log("inside")
 			if($(this).val() == questions[0].correctAnswer){
-				game.correct++;			
+				game.correct++;
 			}else {
 				game.incorrect++;
 			}
     });
-        $.each($("input[name='answer2']:checked"), function () {
+        $.each($("input[name='answer1']:checked"), function () {
         console.log("inside")
         if($(this).val() == questions[1].correctAnswer){
             game.correct++;
@@ -96,7 +97,7 @@ var game = {
         	game.incorrect++;
         }
     });
-        $.each($("input[name='answer3']:checked"), function () {
+        $.each($("input[name='answer2']:checked"), function () {
         console.log("inside")
         if($(this).val() == questions[2].correctAnswer) {
             game.correct++;
@@ -104,7 +105,7 @@ var game = {
         	game.incorrect++;
         }
     });
-        $.each($("input[name='answer4']:checked"), function () {
+        $.each($("input[name='answer3']:checked"), function () {
         console.log("inside")
         if($(this).val() == questions[3].correctAnswer) {
             game.correct++;
@@ -120,7 +121,7 @@ var game = {
         	game.incorrect++;
         }
     });
-        $.each($("input[name='answer4']:checked"), function () {
+        $.each($("input[name='answer5']:checked"), function () {
         console.log("inside")
         if($(this).val() == questions[5].correctAnswer) {
             game.correct++;
@@ -138,6 +139,3 @@ var game = {
 
  		}
  	}
-
-
- 
